2d7d72c38d7fe5408f6ae86cf6fa199e53e95e91
Mike <ampgzo2@gmail.com>
2022-05-06 20:47:00 -0400

    interpolate page-template data

--- 9.5/src/page-template.js
+++ 9.5/src/page-template.js
@@ -1,58 +1,39 @@
 // create the about section
-const generateAbout = aboutText => {
-  if (!aboutText) {
-    return '';
-  }
+
+const managerCard = managerData => {
+  if (!managerData) { return ''; }
 
   return `
-    <section class="my-3" id="about">
-      <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
-      <p>${aboutText}</p>
+    <section class="my-3 card" id="about" style="text-align:center;">
+      <h2 class="text-dark bg-primary p-2 display-inline-block">Manager</h2>
+      <p>Name:${managerData.manager_name}(${managerData.office_number})</p>
+      <p>Email:${managerData.manager_email}</p>
     </section>
   `;
 };
 
-// create the projects section
-const generateProjects = projectsArr => {
-  return `
-    <section class="my-3" id="portfolio">
-      <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
-      <div class="flex-row justify-space-between">
-      ${projectsArr
-        .filter(({ feature }) => feature)
-        .map(({ name, description, languages, link }) => {
-          return `
-          <div class="col-12 mb-2 bg-dark text-light p-3">
-            <h3 class="portfolio-item-title text-light">${name}</h3>
-            <h5 class="portfolio-languages">
-              Built With:
-              ${languages.map(language => language).join(',')}
-            </h5>
-            <p>${description}</p>
-            <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-          </div>
-        `;
-        })
-        .join('')}
+const employeeCard = employeeData => {
+  if (!employeeData) { return ''; }
 
-      ${projectsArr
-        .filter(({ feature }) => !feature)
-        .map(({ name, description, languages, link }) => {
-          console.log(languages);
-          return `
-          <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-            <h3 class="portfolio-item-title text-light">${name}</h3>
-            <h5 class="portfolio-languages">
-              Built With:
-              ${languages.join(', ')}
-            </h5>
-            <p>${description}</p>
-            <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-          </div>
-        `;
-        })
+  return `
+    <section class="my-3 card" id="about" style="text-align:center;">
+      <h2 class="text-dark bg-primary p-2 display-inline-block">${employeeData.employee_type}</h2>
+      <p>Name:${employeeData.employee_name}(${employeeData.employee_id})</p>
+      <p>Email:${employeeData.employee_email}</p>
+      <p>${employeeData.employee_type == 'Engineer' ? 'Github' : 'School'}:${employeeData.employee_custom}</p>
+    </section>
+  `;
+};
+
+
+// create the employee cards
+const employeeCards = employees => {
+  return `
+    <section class="my-3">
+      <div class="flex-row justify-space-evenly">
+      ${employees
+        .map(employee => employeeCard(employee))
         .join('')}
-    
       </div>
     </section>
   `;
@@ -61,7 +42,7 @@ const generateProjects = projectsArr => {
 // export function to generate entire page
 module.exports = templateData => {
   // destructure page data by section
-  const { header, manager } = templateData;
+  const { employees, manager } = templateData;
 
   return `
   <!DOCTYPE html>
@@ -80,18 +61,16 @@ module.exports = templateData => {
   <body>
     <header>
       <div class="container flex-row justify-space-between align-center py-3">
-        <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
+        <h1 class="page-title text-secondary bg-dark py-2 px-3">Team Builder Pro</h1>
         <nav class="flex-row">
-          <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github}">GitHub</a>
+          <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/">GitHub</a>
         </nav>
       </div>
     </header>
-    <main class="container my-5">
-    ${manager}
-    // identification function goes here
-    </main>
+    <main class="container my-5">${managerCard(manager)}</main>
+    ${employeeCards(employees)}
     <footer class="container text-center py-3">
-      <h3 class="text-dark">&copy;2020 by ${header.name}</h3>
+      <h3 class="text-dark">&copy;2020 by Team Builder Pro</h3>
     </footer>
   </body>
   </html>
